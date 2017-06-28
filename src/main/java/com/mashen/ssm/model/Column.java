3f5a51983999342fc2d88f3f36a9d1661148b032
a3311598 <a3311598@qq.com>
2017-06-28 20:13:27 +0800

    Signed-off-by: a3311598 <a3311598@qq.com>

---
+++ src/main/java/com/mashen/ssm/model/Column.java
@@ -0,0 +1,19 @@
+package com.mashen.ssm.model;
+
+public class Column {
+	private String columnname;
+	private String creationtime;
+	public String getColumnname() {
+		return columnname;
+	}
+	public void setColumnname(String columnname) {
+		this.columnname = columnname;
+	}
+	public String getCreationtime() {
+		return creationtime;
+	}
+	public void setCreationtime(String creationtime) {
+		this.creationtime = creationtime;
+	}
+	
+}
