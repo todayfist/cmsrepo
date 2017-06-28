79560e6b508f931a3bbc2c42bff7a9e21cedd43e
a3311598 <a3311598@qq.com>
2017-06-28 10:50:40 +0800

    v1.0
    Signed-off-by: a3311598 <a3311598@qq.com>

---
+++ ssm_1/src/main/java/com/mashen/ssm/controller/UserAction.java
@@ -0,0 +1,14 @@
+package com.mashen.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/user3")
+public class UserAction {
+	@RequestMapping("/login3")
+	public String login(){
+		System.out.println("跳转成功!"); 
+		return "success";
+	}
+}
