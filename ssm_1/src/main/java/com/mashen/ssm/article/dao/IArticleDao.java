f80834d65e15385932cbda6260693f10dfd5377a
aiveecome <471038423@qq.com>
2017-06-30 13:01:51 +0800

    Signed-off-by: aiveecome <471038423@qq.com>

--- ssm_1/src/main/java/com/mashen/ssm/article/dao/IArticleDao.java
+++ ssm_1/src/main/java/com/mashen/ssm/article/dao/IArticleDao.java
@@ -1,5 +1,21 @@
 package com.mashen.ssm.article.dao;
 
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.mashen.ssm.article.model.Article;
+
+@Repository("articleDao")
 public interface IArticleDao {
+	public int add(Article article);
+	
+	public int delete(int id);
+	
+	public int update(Article article);
+	public List<Article> select();
+	public List<Article> selectbyname(String headline);
+	public Article selectbyid(int id);
+	
 
 }
